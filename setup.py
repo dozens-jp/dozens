6f41af8d5686d726f8b30e08e97f282ec3ca21ba
mikamix <tmp@mikamix.net>
2012-05-19 03:10:08 +0900

    （´Д｀）

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
+import os
 from setuptools import setup
 from setuptools import find_packages
 
+version = '0.0.1'
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
 setup(
     name='dozens',
-    version='0.0.1',
+    version=version,
     packages=find_packages(),
-    test_suite='dozens.tests',
-    setup_requires=['nose'],
-    tests_require=['mock', 'coverage'],
+    test_suite='tests',
+    tests_require=['mock'],
+
+    author='mikamix',
+    author_email='tmp@mikamix.net',
+    description='Wrapper for Dozens API',
+    long_description=README,
+    keywords=['dozens'],
+    classifiers=classifiers,
+    license='MIT',
 )
